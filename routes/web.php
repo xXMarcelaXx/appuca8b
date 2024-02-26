d4619a0ab248f93962e97114f5b0951879aab2b7
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2024-02-25 21:59:04 -0600

    login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::middleware(['auth'])->group(function () {
